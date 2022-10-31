e661709ab6770755b5aab1c9198f07b1896883a4
ulugbekusmanovdev <83013051+ulugbekusmanovdev@users.noreply.github.com>
2022-10-31 13:31:29 +0600

    'added'

--- home/models.py
+++ home/models.py
@@ -5,19 +5,24 @@ class Ads(models.Model):
     title = models.CharField(max_length=200,blank=True, null=True, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
 
+    class Meta:
+        verbose_name = 'Объявления'
+        verbose_name_plural = 'Объявления'
+
 
 class Library(models.Model):
-    l_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
-    l_body = models.TextField(blank=True, null=True, verbose_name='Текст')
-    m_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
-    m_body = models.TextField(blank=True, null=True, verbose_name='Текст')
-    os_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
-    os_body = models.TextField(blank=True, null=True, verbose_name='Текст')
-    history_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок')
-    history_body = models.TextField(blank=True, null=True, verbose_name='Текст')
-    history_image = models.ImageField(blank=True, null=True, verbose_name='Картинки')
+    l_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок о библиотеке')
+    l_body = models.TextField(blank=True, null=True, verbose_name='Текст о библиотеке')
+    m_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок миссия')
+    m_body = models.TextField(blank=True, null=True, verbose_name='Текст миссия')
+    os_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок основные зад')
+    os_body = models.TextField(blank=True, null=True, verbose_name='Текст основные зад')
+    history_title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Заголовок история')
+    history_body = models.TextField(blank=True, null=True, verbose_name='Текст история')
+    history_image = models.ImageField(blank=True, null=True, verbose_name='Картинки история')
 
 
     class Meta:
         verbose_name = 'О Библиотеке'
         verbose_name_plural = 'О Библиотеке'
+
