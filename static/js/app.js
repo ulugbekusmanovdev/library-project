c8c5a7c1ff560d2a6f752b9522a2e8def32dd73d
Sammkko <wolf.j2.sam@gmail.com>
2022-10-31 13:02:25 +0300

    Sam

--- static/js/app.js
+++ static/js/app.js
@@ -3,11 +3,12 @@
 const nav = document.getElementById('nav');
 const menu = document.getElementById('menu');
 const menuHeigt = menu.getBoundingClientRect().height;
+const menuH = menuHeigt - 240;
 
 const setScrollListebner = () => {
     const { scrollY } = window;
 
-    if (scrollY >= menuHeigt) {
+    if (scrollY >= menuH) {
         nav.classList.add('sticky');
     } else {
         nav.classList.remove('sticky');
