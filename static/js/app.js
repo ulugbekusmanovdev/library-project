05ee0da41a56e2023cbbfd6a67ca467c3db5ccd2
Sammkko <wolf.j2.sam@gmail.com>
2022-12-05 09:36:45 +0300

    one sec

--- static/js/app.js
+++ static/js/app.js
@@ -5,16 +5,54 @@ const menu = document.getElementById('menu');
 const menuHeigt = menu.getBoundingClientRect().height;
 const menuH = menuHeigt - 240;
 
-const setScrollListebner = () => {
-    const { scrollY } = window;
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const elemenId = window.location.href.match(/#(\w+)$/)[1];
+// 	if(elemenId){
+// 		console.log(elemenId, window.location.href);
+// 		const elementTop = document.getElementById(elemenId).getBoundingClientRect()
+	
+// 		window.scrollTo({
+// 			behavior: "smooth",
+// 			top: elementTop - 400
+// 		})
+// 	}
+	
+// })
 
-    if (scrollY >= menuH) {
-        nav.classList.add('sticky');
-    } else {
-        nav.classList.remove('sticky');
-    }
+
+
+
+
+
+
+const setScrollListebner = () => {
+	const { scrollY } = window;
+
+	if (scrollY >= menuH) {
+		nav.classList.add('sticky');
+	} else {
+		nav.classList.remove('sticky');
+	}
 }
 
 window.addEventListener("scroll", setScrollListebner)
 
 
+
+/* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+function myFunction() {
+	document.getElementById("books_myDropdown").classList.toggle("show");
+}
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function (event) {
+	if (!event.target.matches('.books_dropbtn')) {
+		var dropdowns = document.getElementsByClassName("books_dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
