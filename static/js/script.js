e932b02da6a5660c6423f0233143c353d407bc07
Sammkko <wolf.j2.sam@gmail.com>
2023-01-03 00:02:21 +0300

    Slider

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,37 @@
     })
 
 })(jQuery)
+
+"use strict";
+(function () {
+
+    $(".photo_slick-slider").slick({
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        // autoplay:true,
+        autoplaySpeed: 4000,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: true,
+    })
+
+})(jQuery)
+
+"use strict";
+(function () {
+
+    $(".news_slick-slider").slick({
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        // autoplay:true,
+        autoplaySpeed: 4000,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: true,
+    })
+
+})(jQuery)
