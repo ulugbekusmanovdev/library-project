7f86c8520e2641b7ac9431006b9a4d6faeb88508
ulugbekusmanovdev <83013051+ulugbekusmanovdev@users.noreply.github.com>
2023-01-20 10:06:32 +0600

    web

--- libraries/urls.py
+++ libraries/urls.py
@@ -4,12 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.forLibrarists, name='forLibrarists'),
     path('readers', views.readers, name='readers'),
-    path('direction', views.direction, name='direction'),
-    path('good_to_know', views.good_to_know, name='good_to_know'),
-    path('library_entry', views.library_entry, name='library_entry'),
-    path('reading_rooms', views.reading_rooms, name='reading_rooms'),
-    path('subscription', views.subscription, name='subscription'),
-    path('work_schedule', views.work_schedule, name='work_schedule'),
     path('register', views.registerUser, name='register'),
     path('login', views.loginUser, name='login'),
     path('logout', views.logoutUser, name='logout'),
--- libraries/views.py
+++ libraries/views.py
@@ -87,30 +87,6 @@ def forLibrarists(request):
     return render(request, 'forLibrarists.html', context)
 
 
-def direction(request):
-    return render(request, 'direction.html')
-
-
-def good_to_know(request):
-    return render(request, 'good_to_know.html')
-
-
-def library_entry(request):
-    return render(request, 'library_entry.html')
-
-
-def reading_rooms(request):
-    return render(request, 'reading_rooms.html')
-
-
-def subscription(request):
-    return render(request, 'subscription.html')
-
-
-def work_schedule(request):
-    return render(request, 'work_schedule.html')
-
-
 def readers(request):
     category = request.GET.get('category')
     if category == None:
